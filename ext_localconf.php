69c8136b8161602f08dae1daa850504d60c585e6
David Vogt <david.h.vogt@gmail.com>
2013-03-13 00:08:02 +0100

    ADD: new plugin

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,18 +3,31 @@ if (!defined('TYPO3_MODE')) {
 	die ('Access denied.');
 }
 
+
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-	'USCHI.' . $_EXTKEY,
-	'Person',
+    'USCHI.' . $_EXTKEY,
+	'Languages',
 	array(
         'Language' => 'list',
-		'Person' => 'list',
 	),
 	// non-cacheable actions
 	array(
-		'Language' => '',
-        'Person' => '',
+        'Language' => '',
 	)
+);
+
+
+
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+    'USCHI.' . $_EXTKEY,
+    'Person',
+    array(
+        'Person' => 'list',
+    ),
+    // non-cacheable actions
+    array(
+        'Person' => '',
+    )
 );
 
 ?>
